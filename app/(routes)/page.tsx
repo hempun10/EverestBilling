ffc61ca149e2ddf248b73a357e851aa0020070f4
Hem Pun <hemthegentlmen@gmail.com>
2023-08-27 15:15:01 +0545

    Added loading file

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -6,7 +6,8 @@ import Testominal from "@/components/Testominal/Testominal";
 import Certificate from "@/components/certficate/certificate";
 import CTA from "@/components/cta/CTA";
 
-export default function Home() {
+export default async function Home() {
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
   return (
     <>
       <Hero />
